35f74f4e99255142181bc42e069ef70f02155b57
Ineke Luijten <168184408+InekeL@users.noreply.github.com>
2026-09-02 18:01:16 +0200

    add registration banner

--- scripts/render_course.py
+++ scripts/render_course.py
@@ -22,6 +22,7 @@ from renderers.navbar import (
     render_navbar_meta,
     render_navbar_links,
 )
+from renderers.registration import render_registration
 from renderers.upcoming import render_upcoming
 from renderers.quick_links import render_quick_links
 from renderers.announcements import render_announcements
@@ -34,6 +35,7 @@ def main():
 
     course = validate_course(load_course())
     website = validate_website(load_website())
+    registration = website.get("registration", {})
     events = validate_schedule(load_schedule())
     team = validate_team(load_team())
     announcements = load_announcements()
@@ -51,6 +53,11 @@ def main():
     write_partial(
         "navbar_links.qmd",
         render_navbar_links(website),
+    )
+
+    write_partial(
+    "registration.qmd",
+    render_registration(registration),
     )
 
     write_partial(
